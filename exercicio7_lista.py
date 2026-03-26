85ef8a2c3e0fba2f4bccb4d7079770e179ed93da
SyangSouzaa <syangsouza05@gmail.com>
2026-03-26 19:35:21 -0300

    adicionando o exercicio7

---
+++ exercicio7_lista.py
@@ -0,0 +1,11 @@
+import math
+
+numero = float(input("Num:"))
+
+if numero >= 0:
+    raiz = math.sqrt(numero)
+    print(f"A raiz quadrada de {numero} é igual a {raiz}")
+
+else:
+    print("Impossível extrair raiz de número negativo! ")
+    
